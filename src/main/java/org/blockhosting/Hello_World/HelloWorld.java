366f06facf8d4301b815a37b9920a1fe7f8731e1
cindayra <cindayra@gmail.com>
2017-05-10 16:35:16 +0200

    Koenn!

--- src/main/java/org/blockhosting/Hello_World/HelloWorld.java
+++ src/main/java/org/blockhosting/Hello_World/HelloWorld.java
@@ -4,7 +4,7 @@ import me.koenn.core.command.CommandAPI;
 import org.blockhosting.Hello_World.commands.testCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
+/*
  * Created by cindayra on 5/4/2017.
  */
 public class HelloWorld extends JavaPlugin{
--- src/main/java/org/blockhosting/Hello_World/PlayerListener.java
+++ src/main/java/org/blockhosting/Hello_World/PlayerListener.java
@@ -1,6 +1,8 @@
 package org.blockhosting.Hello_World;
 
+
 import me.koenn.core.misc.Timer;
+//import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,7 +10,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-/**
+//import org.bukkit.event.player.PlayerMoveEvent;
+
+/*
  * Created by cindayra on 5/6/2017.
  */
 public class PlayerListener implements Listener {
@@ -22,11 +26,10 @@ public class PlayerListener implements Listener {
 
         Player player = event.getPlayer();
 
-        player.sendMessage(ChatColor.RED + "Ethan is a scrub");
+        player.sendMessage(ChatColor.AQUA + "Ethan is a scrub");
         new Timer(200, HelloWorld.getInstance()).start(() -> {
             player.kickPlayer("Sorry, Ethan told me to");
         });
-
 
 
     }
@@ -35,10 +38,30 @@ public class PlayerListener implements Listener {
     public void joinDenier(PlayerJoinEvent denied) {
         Player player = denied.getPlayer();
 
-        if (player.getDisplayName() == "Derpproduction") {
+        if (player.getPlayerListName() == "CraftPlayer{name=Derpproduction}") {
             player.kickPlayer("Bye Ethan!");
         }
     }
 
+
+/*    @EventHandler
+    public void moving(PlayerMoveEvent move) {
+        Player player = move.getPlayer();
+
+
+
+        player.sendMessage(ChatColor.DARK_RED + "Don't " + ChatColor.BOLD + "move!");
+
+    }*/
+
+
+/*    @EventHandler
+    public void welcome(PlayerJoinEvent welcome) {
+
+        Player player = welcome.getPlayer();
+
+        Bukkit.broadcastMessage("Welcome " + player + " to the official BlockGaming server");
+
+    }*/
 
 }
