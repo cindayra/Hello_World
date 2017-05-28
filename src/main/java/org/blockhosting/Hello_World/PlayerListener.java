a6b408fbb867bfed939c7385ff15a37d4f7f4522
cindayra <cindayra@gmail.com>
2017-05-28 20:38:10 +0200

    Koenn!

--- src/main/java/org/blockhosting/Hello_World/PlayerListener.java
+++ src/main/java/org/blockhosting/Hello_World/PlayerListener.java
@@ -1,14 +1,9 @@
 package org.blockhosting.Hello_World;
 
-
-import me.koenn.core.listeners.PlayerJoinListener;
-import me.koenn.core.misc.Timer;
 import me.koenn.core.misc.Title;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /*
@@ -16,51 +11,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
  */
 public class PlayerListener implements Listener {
 
-    public PlayerListener(HelloWorld plugin) {
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
-    }
-
-    @EventHandler
-    public void onThrow(PlayerEggThrowEvent event) {
-
-        Player player = event.getPlayer();
-
-        player.sendMessage(ChatColor.AQUA + "Ethan is a scrub");
-        new Timer(200, HelloWorld.getInstance()).start(() -> {
-            player.kickPlayer("Sorry, Ethan told me to");
-        });
-
-
-    }
-
-
     @EventHandler
     public void welcome(PlayerJoinEvent Welcome){
 
         Player player = Welcome.getPlayer();
 
-        new Title("Welcome " + player.getName(), " to &aBlock&bGaming &fenjoy your stay!").send(player);
-
-    }
-
-
-    @EventHandler
-    public void byeEthan (PlayerJoinEvent bye) {
-
-        Player player = bye.getPlayer();
-
-        String Ethan = player.getName();
-        String controll = "Cindayra";
-
-        if (Ethan == controll) {
-
-            new Timer(40, HelloWorld.getInstance()).start(() -> {
-
-            player.kickPlayer("See you later, Ethan!");
-            });
-
-        }
-
+        new Title("Welcome " + player.getName(), "to &aBlock&bGaming &fenjoy your stay!").send(player);
 
     }
 
