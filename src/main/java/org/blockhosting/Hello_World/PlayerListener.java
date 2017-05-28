6aa00fe6abe5d451d79758184ea63c01269b30d7
cindayra <cindayra@gmail.com>
2017-05-28 20:44:17 +0200

    Koenn!

--- src/main/java/org/blockhosting/Hello_World/PlayerListener.java
+++ src/main/java/org/blockhosting/Hello_World/PlayerListener.java
@@ -1,9 +1,13 @@
 package org.blockhosting.Hello_World;
 
+
+import me.koenn.core.misc.Timer;
 import me.koenn.core.misc.Title;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /*
@@ -11,12 +15,17 @@ import org.bukkit.event.player.PlayerJoinEvent;
  */
 public class PlayerListener implements Listener {
 
+    public PlayerListener(HelloWorld plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+
     @EventHandler
     public void welcome(PlayerJoinEvent Welcome){
 
         Player player = Welcome.getPlayer();
 
-        new Title("Welcome " + player.getName(), "to &aBlock&bGaming &fenjoy your stay!").send(player);
+        new Title("Welcome " + player.getName(), "to &aBlock&bGaming&f, enjoy your stay!").send(player);
 
     }
 
